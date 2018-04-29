20b9cb26444235263c7e7b24ff23efb5718a066c
Daniel Foster <danieljfoster87@gmail.com>
2018-04-28 21:01:02 -0400

    setting up server

--- server.js
+++ server.js
@@ -2,3 +2,22 @@ var express = require("express");
 var bodyparser = require("body-parser");
 var path = require("path");
 
+// create server with express
+var app = express();
+
+// set up port
+var PORT = process.env.PORT || 3000;
+
+// set up middleware (bodyParser)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+// set up routes
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// turn on server
+app.listen(PORT, function() {
+  console.log("you are now listening on port: " + PORT);
+});
